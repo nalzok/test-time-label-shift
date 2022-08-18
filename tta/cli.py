fix numerical issues

tta/cli.py
@@ -101,12 +101,13 @@ def cli(dataset_name: str,
             with jnp.printoptions(precision=3):
                 print(f'Calibration step {step + 1}, loss: {unreplicate(loss)}')
 
+    # Sync the batch statistics across replicas so that evaluation is deterministic.
     state = state.replace(batch_stats=cross_replica_mean(state.batch_stats))
 
 
     print('===> Inducing Source Label Prior')
     N = 0
-    source_prior = jnp.zeros((C * K,))
+    source_prior = jnp.zeros((device_count, C * K))
     calibration_loader = FastDataLoader(calibration, train_batch_size, num_workers, generator)
     for X, _, _ in calibration_loader:
         remainder = X.shape[0] % device_count
@@ -115,11 +116,11 @@ def cli(dataset_name: str,
 
         N += X.shape[0]
         X = jnp.array(X).reshape(device_count, -1, *X.shape[1:])
-        source_prior = source_prior + unreplicate(induce_step(state, X))
+        source_prior = source_prior + induce_step(state, X)
 
     params = state.params.unfreeze()
-    params['source_prior'] = replicate(source_prior / N)
-    state = state.replace(params=flax.core.frozen_dict.unfreeze(params))
+    params['source_prior'] = source_prior / N
+    state = state.replace(params=flax.core.frozen_dict.freeze(params))
 
 
     print('===> Adapting & Evaluating')

tta/train.py
@@ -98,7 +98,7 @@ def adapt_step(state: TrainState, X: jnp.ndarray) -> TrainState:
 
     def cond_fun(val):
         prev_objective, objective, iters, _ = val
-        return jnp.logical_or(iters < 2,    # due to numerical instability, the first iteration might decrease the objective
+        return jnp.logical_or(objective < 0,    # the initial iterations might decrease the objective due to numerical instability
                               jnp.logical_and(iters < 1e4,
                                               objective - prev_objective > 1e-2))
 
@@ -113,15 +113,17 @@ def adapt_step(state: TrainState, X: jnp.ndarray) -> TrainState:
         # M step
         target_prior = jax.lax.pmean(jnp.mean(target_likelihood, axis=0), axis_name='batch')
 
-        w = source_prior / target_prior
-        objective_i = jnp.log(jnp.sum(w * source_likelihood, axis=-1))
+        log_w = jnp.log(source_prior) - jnp.log(target_prior)
+        objective_i = jax.nn.logsumexp(log_w, axis=-1, b=source_likelihood)
         objective = jax.lax.psum(jnp.sum(objective_i), axis_name='batch')
 
         return prev_objective, objective, iters + 1, target_prior
 
     _, _, _, target_prior = jax.lax.while_loop(cond_fun, body_fun, init_val)
 
-    state.params['target_prior'] = target_prior
+    params = state.params.unfreeze()
+    params['target_prior'] = target_prior
+    state = state.replace(params=flax.core.frozen_dict.freeze(params))
 
     return state
 