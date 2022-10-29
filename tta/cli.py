fix conditional distribution computation

tta/cli.py
@@ -651,7 +651,7 @@ def adapt(
     auc_sweep = jnp.empty(len(eval_splits))
     accuracy_sweep = jnp.empty(len(eval_splits))
     norm_sweep = jnp.empty(len(eval_splits))
-    for i, (joint_YZ, eval_) in enumerate(eval_splits):
+    for i, (joint_M, eval_) in enumerate(eval_splits):
         # happens on the source domain when train_fraction = 1.0
         if len(eval_) == 0:
             mean_sweep = mean_sweep.at[i].set(jnp.nan)
@@ -669,15 +669,15 @@ def adapt(
             else "(unseen)"
         )
 
-        joint_YZ = jnp.array(joint_YZ)
+        joint_M = jnp.array(joint_M)
         flip_prob = jnp.array(
             [
                 [1 - dataset_label_noise, dataset_label_noise],
                 [dataset_label_noise, 1 - dataset_label_noise],
             ]
         )
-        joint = flip_prob[:, :, jnp.newaxis] * joint_YZ  # P(Y_tilde, Y, Z)
-        prob = joint / jnp.sum(joint, axis=1, keepdims=True)
+        joint = flip_prob[:, :, jnp.newaxis] * joint_M  # P(Y_tilde, Y, Z)
+        prob = joint / jnp.sum(joint, axis=(0, 2), keepdims=True)
         prob = prob[:, 1, :]  # P(Y=1|Y_tilde, Z)
 
         # using shuffle=True so that Y contains multiple classes, otherwise AUC is not defined
@@ -719,7 +719,7 @@ def adapt(
                 )
             elif label == "Oracle":
                 prior = state.prior.unfreeze()
-                prior["target"] = replicate(joint_YZ.flatten())
+                prior["target"] = replicate(joint_M.flatten())
                 state = state.replace(prior=flax.core.frozen_dict.freeze(prior))
             elif label == "Unadapted":
                 prior = state.prior.unfreeze()
@@ -736,7 +736,7 @@ def adapt(
             epoch_score = epoch_score.at[offset:offset+N].set(score.flatten())
             hits += unreplicate(hit)
             prior = unreplicate(state.prior["target"]).reshape((C, K))
-            norm += N * jnp.linalg.norm(prior - joint_YZ)
+            norm += N * jnp.linalg.norm(prior - joint_M)
 
             offset += N
 

tta/datasets/chexpert.py
@@ -63,7 +63,7 @@ class MultipleDomainCheXpert(MultipleDomainDataset):
 
         domains = [None for _ in confounder_strength]
 
-        labels["YZ"] = 2 * labels[Y_column] + labels[Z_column]
+        labels["M"] = 2 * labels[Y_column] + labels[Z_column]
         mask = np.ones(len(labels.index), dtype=bool)
 
         # Sample source domains
@@ -71,17 +71,17 @@ class MultipleDomainCheXpert(MultipleDomainDataset):
             if i not in train_domains:
                 continue
 
-            joint_YZ = torch.from_numpy(strength * confounder1 + (1-strength) * confounder2)
-            joint_YZ_flatten = joint_YZ.numpy().flatten()
-            quota = labels["YZ"].loc[mask].value_counts(ascending=True).to_numpy() // 2
+            joint_M = torch.from_numpy(strength * confounder1 + (1-strength) * confounder2)
+            joint_M_flatten = joint_M.numpy().flatten()
+            quota = labels["M"].loc[mask].value_counts(ascending=True).to_numpy() // 2
             quota = torch.from_numpy(quota)
-            count = torch.round(torch.min(quota/joint_YZ_flatten)*joint_YZ_flatten)
+            count = torch.round(torch.min(quota/joint_M_flatten)*joint_M_flatten)
             count = count.long().reshape((2, 2))
-            joint_YZ = count / torch.sum(count)
+            joint_M = count / torch.sum(count)
 
             domain, in_sample = self.sample(embeddings, labels, mask, count)
             mask &= ~labels.index.isin(in_sample)
-            domains[i] = (joint_YZ, domain)
+            domains[i] = (joint_M, domain)
 
 
         # Sample target domains
@@ -89,16 +89,16 @@ class MultipleDomainCheXpert(MultipleDomainDataset):
             if i in train_domains:
                 continue
 
-            joint_YZ = torch.from_numpy(strength * confounder1 + (1-strength) * confounder2)
-            joint_YZ_flatten = joint_YZ.numpy().flatten()
-            quota = labels["YZ"].loc[mask].value_counts(ascending=True).to_numpy()
+            joint_M = torch.from_numpy(strength * confounder1 + (1-strength) * confounder2)
+            joint_M_flatten = joint_M.numpy().flatten()
+            quota = labels["M"].loc[mask].value_counts(ascending=True).to_numpy()
             quota = torch.from_numpy(quota)
-            count = torch.round(torch.min(quota/joint_YZ_flatten)*joint_YZ_flatten)
+            count = torch.round(torch.min(quota/joint_M_flatten)*joint_M_flatten)
             count = count.long().reshape((2, 2))
-            joint_YZ = count / torch.sum(count)
+            joint_M = count / torch.sum(count)
 
             domain, _ = self.sample(embeddings, labels, mask, count)
-            domains[i] = (joint_YZ, domain)
+            domains[i] = (joint_M, domain)
 
         self.domains = domains
 
@@ -112,7 +112,7 @@ class MultipleDomainCheXpert(MultipleDomainDataset):
 
         for Y in range(2):
             for Z in range(2):
-                masked = labels.loc[mask & (labels["YZ"] == 2 * Y + Z)]
+                masked = labels.loc[mask & (labels["M"] == 2 * Y + Z)]
                 indices = masked.sample(int(count[Y, Z]), random_state=42)
                 in_sample.update(indices.index)
 

tta/datasets/waterbirds.py
@@ -65,6 +65,6 @@ class MultipleDomainWaterbirds(MultipleDomainDataset):
             for label in counter:
                 y_count[label] += counter[label]
             y_freq = y_count / len(domain)
-            joint_YZ = y_freq[:, np.newaxis] * conditional
+            joint_M = y_freq[:, np.newaxis] * conditional
 
-            self.domains.append((joint_YZ, domain))
+            self.domains.append((joint_M, domain))