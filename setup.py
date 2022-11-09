6746617a882e3fafc7d84da6d95864065371257a
Qingyao Sun <qys@uchicago.edu>
2022-11-08 21:52:15 -0600

    specify dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name="Test time label shift adaptation",
     version="0.1",
     packages=find_packages(),
+    install_requires=required,
 )
